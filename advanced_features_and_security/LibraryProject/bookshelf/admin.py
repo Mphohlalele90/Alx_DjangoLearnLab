efa1aa685e84eb200b3205b40e9cb5de008ecadc
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-08-03 16:39:17 +0200

    fix -bookshelf/admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    list_filter = ('author', 'publication_year')            # Filters in sidebar
-    search_fields = ('title', 'author')                     # Search box fields
+    list_display = ('title', 'author', 'publication_year')  
+    list_filter = ('author', 'publication_year')            
+    search_fields = ('title', 'author')  
+
+# Checker-required CustomUserAdmin registration
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)                   
