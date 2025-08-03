643648148ca2463666b0c7ba53bf3316f681ffea
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-08-03 21:09:04 +0200

    fix -forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -8,3 +8,6 @@ class BookForm(forms.ModelForm):
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(max_length=200, required=False)
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=False)
