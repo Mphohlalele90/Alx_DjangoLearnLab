00fc5bba12cfbd9c041e8ec66f6a8626e425c5ba
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-08-03 21:28:09 +0200

    fix - views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookSearchForm, BookForm, ExampleForm
+from .forms import BookSearchForm, BookForm, ExampleForm 
+
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
