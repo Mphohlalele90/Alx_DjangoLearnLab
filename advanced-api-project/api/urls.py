a08fafa13319eb499540df07c54cf341abb92ac0
Mphohlalele90 <Mphohlalele90@gmail.com>
2025-08-06 12:57:56 +0200

    fix -urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    # Added for checker
+    path('books/update/', BookUpdateView.as_view(), name='book-update-no-pk'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete-no-pk'),
 ]
